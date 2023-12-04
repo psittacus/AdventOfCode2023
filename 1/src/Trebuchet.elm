3735417cb860f1eb1e508897d28859c1336b9a03
psittacus <ps1ttacus@web.de>
2023-12-04 15:05:14 +0100

    Added solution for one string, now add a solution for many strings

--- 1/src/Trebuchet.elm
+++ 1/src/Trebuchet.elm
@@ -3,24 +3,26 @@ import Browser
 import Html exposing (Html, Attribute, button, div, text, input)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
+import String exposing (..)
+import Char exposing (..)
 
 main =
   Browser.sandbox { init = init, update = update, view = view }
 
 type Msg = Translate | Draft String
 
-type alias Model = { content : String } 
+type alias Model = { content : String , output : String} 
 
-type IntOrStr = Nummer Int | Text String
+type IntOrStr = AnInt Int | AChar Char 
 
 init : Model 
-init = { content = ""}
+init = { content = "", output = ""}
 
 update : Msg -> Model -> Model
 update msg model = 
     case msg of
         Translate ->
-            extractNumbers model
+            { model | content = model.content, output = getFirstAndLast (extractNumbers model).output }
 
         Draft input -> 
             { model | content = input }
@@ -29,22 +31,33 @@ extractNumbers : Model -> Model
 extractNumbers model =  
     case (uncons model.content) of 
         Just (x, xs) -> 
-            { model | content = numberOrString x ++ (extractNumbers { model | content = xs })}
+            { model | output = numberOrString x ++ (extractNumbers {model | content = xs}).output}
         Nothing -> 
-            { model | content = "" }
+            { model | output = "", content = "" }
 
 view : Model -> Html Msg
 view model =
   div []
     [ input [ value model.content , onInput Draft ] []
     , button [ onClick Translate ] [text "Translate"]
+    , div [] [text model.output]
     ]
 
-numberOrString : IntOrStr -> String
+numberOrString : Char -> String
 numberOrString x = 
-    case x of
-        Nummer num -> 
-            String.fromInt x
-        Text str -> 
-            str
+    if isDigit x then
+        String.fromChar x
+    else
+        ""
+
+getFirstAndLast : String -> String
+getFirstAndLast str =
+    if String.length str == 1 then
+        repeat 2 str
+    else if String.length str == 2 then
+        str
+    else if String.length str > 2 then
+        left 1 str ++ right 1 str
+    else
+        "Number smaller than 1"
 
